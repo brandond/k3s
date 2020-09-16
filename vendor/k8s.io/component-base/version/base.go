9cfbb42b63d25d792c57740a89f23a3cc146ef81
Brad Davidson <brad.davidson@rancher.com>
2020-09-16 14:51:46 -0700

    Update codegen for v1.17.12-k3s1
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "17"
-	gitVersion   = "v1.17.11-k3s1"
-	gitCommit    = "51cfdf7fa2caf99d6ee48a3a5257d4177b8b4665"
+	gitVersion   = "v1.17.12-k3s1"
+	gitCommit    = "35634b9ec2fde16244c4dd331c808f700ce85bcb"
 	gitTreeState = "clean"
-	buildDate = "2020-08-13T18:43:40Z"
+	buildDate = "2020-09-16T21:43:11Z"
 )
