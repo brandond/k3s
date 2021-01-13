ef5fb14ab7ed676380ebcf5efa360f51341eab78
Brad Davidson <brad.davidson@rancher.com>
2021-01-13 11:25:32 -0800

    Update Kubernetes to v1.19.6-k3s1
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "19"
-	gitVersion   = "v1.19.5-k3s1"
-	gitCommit    = "aecefed287cf995dcb5c4803dd422ee6bad621bf"
+	gitVersion   = "v1.19.6-k3s1"
+	gitCommit    = "6d8e29c7b1d4256277b12066b2ecb7bc4fee4e26"
 	gitTreeState = "clean"
-	buildDate = "2020-12-09T22:46:17Z"
+	buildDate = "2021-01-13T19:20:59Z"
 )
